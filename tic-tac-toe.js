48e4d272fe85fa34c0c5911afa2036dc548c37ae
Jon-Michael <jof8888@gmail.com>
2022-10-12 01:30:50 -0500

    Completed Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,38 @@
 window.onload = function(){
+    let square = document.querySelectorAll("#board > div");
+    const state =[];
+    
 
-    var square = document.querySelectorAll("#board > div");
+    square.forEach(function(sq,index){
+        sq.classList.add("square");
+        
+        sq.addEventListener('click',function(e){
+            if(state.length==0)
+            {
+                sq.innerHTML= "X";
+                e.target.classList.add('X');
+                state.push("X");
+            }
+            
+            else if(state[state.length-1]=='O')
+            {
+                sq.innerHTML= "X";
+                e.target.classList.add('X');
+                state.push("X");
+            }
 
-    square.forEach((element) =>{
-        element.classList.add("square");
-    });
+            else
+            {
+                sq.innerHTML= "O";
+                e.target.classList.add('O');
+                state.push("O");
+            }
+
+
+        });
+
+    }); 
+
+
 
 }
